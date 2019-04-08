16ae0330103c59fc015a9cfa1f3035f2de49cc54
Petter Ruud <petter.ruud@nav.no>
2019-04-08 13:58:30 +0200

    NOJIRA - js -> jsx

--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Dashboard from './Dashboard'
+import Dashboard from './Dashboard';
 
 import './App.css';
 
 class App extends Component {
-
   // beregningdato
   // vilkarId
   // aktorId
   // http://localhost:3000/aktorId/1/beregningdato/2019-08-04/vilkarId/2
   render() {
     return (
+      <div>
+        <header>
+          dsfdsfd
+        </header>
         <Router>
           <Route exact path="/aktorId/:aktorId/beregningdato/:beregningdato/vilkarId/:vilkarid" component={Dashboard} />
         </Router>
+      </div>
     );
   }
 }
