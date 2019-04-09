20ea613a3441ade39546305242465bf39ee32595
Petter Ruud <petter.ruud@nav.no>
2019-04-09 17:42:49 +0200

    NOJIRA - oppdaterer styling

--- src/Containers/Dashboard.jsx
+++ src/Containers/Dashboard.jsx
@@ -27,42 +27,54 @@ const Dashboard = (location) => {
     <div className="grid">
 
       <div className="arbeidsgivere">
-        <div className="maaned">&laquo;</div>
-        <div className="arbeidsgiver">
-          <Undertittel>Arbeidsgiver</Undertittel>
+        <div className="item nav">
+          &laquo; &raquo;
+        </div>
+        <div className="item arbeidsgiver">
+          <Undertittel>Bacon AS</Undertittel>
+          <EtikettLiten>Org nr</EtikettLiten>
+          <Element>910047388</Element>
 
         </div>
-        <div className="arbeidsgiver">
-          <Undertittel>Arbeidsgiver</Undertittel>
+        <div className="item arbeidsgiver">
+          <Undertittel>Bacon AS</Undertittel>
+          <EtikettLiten>Org nr</EtikettLiten>
+          <Element>910047388</Element>
 
         </div>
-        <div className="arbeidsgiver">
-          <Undertittel>Arbeidsgiver</Undertittel>
+        <div className="item arbeidsgiver">
+          <Undertittel>Bacon AS</Undertittel>
+          <EtikettLiten>Org nr</EtikettLiten>
+          <Element>910047388</Element>
 
         </div>
-        <div className="arbeidsgiver">
-          <Undertittel>Arbeidsgiver</Undertittel>
+        <div className="item arbeidsgiver">
+          <Undertittel>Bacon AS</Undertittel>
+          <EtikettLiten>Org nr</EtikettLiten>
+          <Element>910047388</Element>
 
         </div>
       </div>
-
-      {data.arbeidsInntektMaaned.map(maaned => (
-        <div className="maaned">
-          <div key={maaned.aarMaaned} className="--navn">
-            {maaned.aarMaaned}
-          </div>
-          {maaned.arbeidsInntektInformasjon.arbeidsgivere.map(arbeidsgiver => (
-            <div key={1} className="inntekter">
-              {arbeidsgiver.inntektListe.map(inntekt => (
-                <div key={1} className="inntekt">
-                  <EtikettLiten>{inntekt.header}</EtikettLiten>
-                  <Element>{inntekt.beloep}</Element>
-                </div>
-              ))}
+      <div className="maaneder">
+        {data.arbeidsInntektMaaned.map(maaned => (
+          <div key={maaned.aarMaaned} className="maaned">
+            <div className="item maanednavn">
+              <Element>{maaned.aarMaaned}</Element>
             </div>
-          ))}
-        </div>
-      ))}
+            {maaned.arbeidsInntektInformasjon.arbeidsgivere.map(arbeidsgiver => (
+              <div key={1} className="item inntekter">
+                {arbeidsgiver.inntektListe.map(inntekt => (
+                  <div key={1} className={`inntekt ${arbeidsgiver.identifikator}`}>
+
+                    <EtikettLiten>{inntekt.header}</EtikettLiten>
+                    <Element>{inntekt.beloep}</Element>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
