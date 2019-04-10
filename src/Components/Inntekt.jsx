1169e633780f79ea84f8476783fb6763a8f8392d
Nico Kuijlen <nico.kuijlen@nav.no>
2019-04-10 13:03:41 +0200

    Bruk ekspandert panel for inntekt

--- src/Components/Inntekt.jsx
+++ src/Components/Inntekt.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Element, EtikettLiten } from 'nav-frontend-typografi';
+import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
+
+const sumInntekter = inntekter => inntekter.reduce((acc, val) => acc + val.beloep, 0);
+
+const formatertPengesum = tall => new Intl.NumberFormat('nb-NO', { style: 'currency', currency: 'NOK' }).format(tall);
 
 const Inntekt = ({ inntekter }) => (
-  <div className="item inntekter">
+  <Ekspanderbartpanel className="item inntekter" tittel={formatertPengesum(sumInntekter(inntekter))} tittelProps="element">
     {inntekter.map(inntekt => (
       <div key={inntekt.beskrivelse} className="inntekt">
-        <EtikettLiten>{inntekt.header}</EtikettLiten>
-        <Element>{inntekt.beloep}</Element>
+        <EtikettLiten>{inntekt.beskrivelse}</EtikettLiten>
+        <Element className="inntektbeloep">{formatertPengesum(inntekt.beloep)}</Element>
       </div>
     ))}
-  </div>
+  </Ekspanderbartpanel>
 );
 
 Inntekt.propTypes = {
