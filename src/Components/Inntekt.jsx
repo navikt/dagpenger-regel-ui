97f207921635ecb110a82623d6774bd40503eca9
Nico Kuijlen <nico.kuijlen@nav.no>
2019-04-10 15:01:50 +0200

    WIP resizing

--- src/Components/Inntekt.jsx
+++ src/Components/Inntekt.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Element, EtikettLiten } from 'nav-frontend-typografi';
@@ -9,26 +9,37 @@ const sumInntekter = inntekter => inntekter.reduce((acc, val) => acc + val.beloe
 const formatertPengesum = tall => new Intl.NumberFormat('nb-NO', { style: 'currency', currency: 'NOK' }).format(tall);
 
 const resize = (rowId, columnId) => {
-  const newHeight = (document.querySelector(`.prefix${rowId}${columnId}`) || {}).height;
+  const selector = document.querySelector(`[class^=prefix${rowId}${columnId}]`);
+  const newHeight = (selector || {}).height;
   document.querySelectorAll(`.prefix${rowId}`).forEach((el) => { el.style.height = newHeight; });
   return newHeight;
 };
 
-const Inntekt = ({ inntekter, rowId, columnId }) => (
-  <div className={classNames('item', 'inntekter', `prefix${rowId}`, `prefix${rowId}${columnId}`)} style={{ height: resize(rowId, columnId) }}>
-    <Ekspanderbartpanel
-      tittel={formatertPengesum(sumInntekter(inntekter))}
-      tittelProps="element"
-    >
-      {inntekter.map(inntekt => (
-        <div key={inntekt.beskrivelse} className="inntekt">
-          <EtikettLiten>{inntekt.beskrivelse}</EtikettLiten>
-          <Element className="inntektbeloep">{formatertPengesum(inntekt.beloep)}</Element>
-        </div>
-      ))}
-    </Ekspanderbartpanel>
-  </div>
-);
+const Inntekt = ({ inntekter, rowId, columnId }) => {
+  const [data, setData] = useState(100);
+
+  useEffect(() => {
+    setData(resize(rowId, columnId));
+  });
+
+
+  return (
+    <div className={classNames('item', 'inntekter', `prefix${rowId}`, `prefix${rowId}${columnId}`)} style={{ height: `${data}px` }}>
+      <Ekspanderbartpanel
+        onClick={() => setData(resize(rowId, columnId))}
+        tittel={formatertPengesum(sumInntekter(inntekter))}
+        tittelProps="element"
+      >
+        {inntekter.map(inntekt => (
+          <div key={inntekt.beskrivelse} className="inntekt">
+            <EtikettLiten>{inntekt.beskrivelse}</EtikettLiten>
+            <Element className="inntektbeloep">{formatertPengesum(inntekt.beloep)}</Element>
+          </div>
+        ))}
+      </Ekspanderbartpanel>
+    </div>
+  );
+};
 
 Inntekt.propTypes = {
   inntekter: PropTypes.arrayOf(PropTypes.shape({
