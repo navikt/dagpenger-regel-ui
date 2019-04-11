3d1527e0b351d6d71b8b45863a22ac10acbcbb0b
Nico Kuijlen <nico.kuijlen@nav.no>
2019-04-11 15:59:00 +0200

    Gjør inntekter redigerbare

--- src/Components/Inntekt.jsx
+++ src/Components/Inntekt.jsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Element, EtikettLiten } from 'nav-frontend-typografi';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
+import { Input } from 'nav-frontend-skjema';
+import { Knapp } from 'nav-frontend-knapper';
+import { Formik, Form, Field } from 'formik';
 
 const sumInntekter = inntekter => inntekter.reduce((acc, val) => acc + val.beloep, 0);
 
 const formatertPengesum = tall => new Intl.NumberFormat('nb-NO', { style: 'currency', currency: 'NOK' }).format(tall);
 
-const Inntekt = ({ inntekter, rowId, columnId }) => (
-  <div className={`item inntekter inntekter--${rowId}--${columnId}`}>
-    <Ekspanderbartpanel
-        // onClick={() => setData(resize(rowId, columnId))}
-      tittel={formatertPengesum(sumInntekter(inntekter))}
-      tittelProps="element"
-    >
-      {inntekter.map(inntekt => (
-        <div key={inntekt.beskrivelse} className="inntekt">
-          <EtikettLiten>{inntekt.beskrivelse}</EtikettLiten>
-          <Element className="inntektbeloep">{formatertPengesum(inntekt.beloep)}</Element>
-        </div>
-      ))}
-    </Ekspanderbartpanel>
+const InputField = ({
+  beskrivelse, field, form, ...props
+}) => (<Input label={beskrivelse} {...field} value={field.value} />);
+
+const ReadOnlyField = ({
+  beskrivelse, field, form, ...props
+}) => (
+  <div>
+    <EtikettLiten>{beskrivelse}</EtikettLiten>
+    <Element className="inntektbeloep">{formatertPengesum(field.value)}</Element>
   </div>
 );
+
+
+const Inntekt = ({ inntekter, rowId, columnId }) => {
+  const [readOnly, setReadOnly] = useState(true);
+
+  return (
+    <div className={`item inntekter inntekter--${rowId}--${columnId}`}>
+      <Ekspanderbartpanel
+        // onClick={() => setData(resize(rowId, columnId))}
+        tittel={formatertPengesum(sumInntekter(inntekter))}
+        tittelProps="element"
+      >
+        <Knapp onClick={() => setReadOnly(!readOnly)}>Rediger</Knapp>
+        {inntekter.map(inntekt => (
+          <div key={inntekt.beskrivelse} className="inntekt">
+            <Field beskrivelse={inntekt.beskrivelse} name={`${rowId}_${columnId}_${inntekt.beskrivelse}`} component={readOnly ? ReadOnlyField : InputField} />
+          </div>
+        ))}
+      </Ekspanderbartpanel>
+    </div>
+  );
+};
 
 Inntekt.propTypes = {
   inntekter: PropTypes.arrayOf(PropTypes.shape({
--- src/Containers/Dashboard.jsx
+++ src/Containers/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Formik, Form } from 'formik';
 import Arbeidsgiver from '../Components/Arbeidsgiver';
 import Maaned from '../Components/Maaned';
 import Inntekt from '../Components/Inntekt';
@@ -18,6 +19,16 @@ const findArbeidsgivere = (data) => {
     .sort((first, second) => second.identifikator - first.identifikator);
 };
 
+const mapToFieldValues = (data) => {
+  const x = data.arbeidsInntektMaaned.reduce((acc, maaned) => (
+    {
+      ...acc,
+      ...maaned.arbeidsInntektInformasjon.inntektListe
+        .reduce((acc2, inntekt) => ({ ...acc2, ...{ [`${inntekt.virksomhet.identifikator}_${maaned.aarMaaned}_${inntekt.beskrivelse}`]: inntekt.beloep } }), {}),
+    }), {});
+  return x;
+};
+
 const Dashboard = (location) => {
   // const routeParams = location.match.params;
   // const { aktorId, beregningdato, vedtakId, inntektId } = routeParams;
@@ -29,36 +40,45 @@ const Dashboard = (location) => {
       const result = await axios(
         'http://localhost:3000/mock/flereinntekter.json',
       );
-
       setData({ arbeidsgivere: findArbeidsgivere(result.data), ...result.data });
     };
 
     getMock();
+    // console.log(data.arbeidsinntektMaaned[0].arbeidsInntektInformasjon.inntektListe[0].beloep)
   }, []);
 
   return (
-    <div className="grid">
-      {data.arbeidsgivere.map(arbeidsgiver => (
-        <Arbeidsgiver key={arbeidsgiver.identifikator} arbeidsgiver={arbeidsgiver} />
-      ))}
-      {data.arbeidsInntektMaaned.map(maaned => (
-        <>
-          <Maaned key={maaned.aarMaaned} maaned={maaned.aarMaaned} />
-          {data.arbeidsgivere.map((arbeidsgiver) => {
-            const inntekter = maaned.arbeidsInntektInformasjon.inntektListe
-              .filter(inntekt => inntekt.virksomhet.identifikator === arbeidsgiver.identifikator);
-            return (
-              <Inntekt
-                rowId={arbeidsgiver.identifikator}
-                columnId={maaned.aarMaaned}
-                key={arbeidsgiver.identifikator}
-                inntekter={inntekter}
-              />
-            );
-          })}
-        </>
-      ))}
-    </div>
+    <Formik
+      enableReinitialize
+      initialValues={mapToFieldValues(data)}
+      onSubmit={undefined}
+    >
+      <Form>
+        <div className="grid">
+          {data.arbeidsgivere.map(arbeidsgiver => (
+            <Arbeidsgiver key={arbeidsgiver.identifikator} arbeidsgiver={arbeidsgiver} />
+          ))}
+
+          {data.arbeidsInntektMaaned.map(maaned => (
+            <>
+              <Maaned key={maaned.aarMaaned} maaned={maaned.aarMaaned} />
+              {data.arbeidsgivere.map((arbeidsgiver) => {
+                const inntekter = maaned.arbeidsInntektInformasjon.inntektListe
+                  .filter(inntekt => inntekt.virksomhet.identifikator === arbeidsgiver.identifikator);
+                return (
+                  <Inntekt
+                    rowId={arbeidsgiver.identifikator}
+                    columnId={maaned.aarMaaned}
+                    key={arbeidsgiver.identifikator}
+                    inntekter={inntekter}
+                  />
+                );
+              })}
+            </>
+          ))}
+        </div>
+      </Form>
+    </Formik>
   );
 };
 
