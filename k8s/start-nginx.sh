4aa557d00e8bbda67ced510660ebfc4288b59c9d
Geir André Lund <geir.andre.lund@nav.no>
2019-04-30 06:47:11 +0200

    Asset caching - debug headers

--- k8s/start-nginx.sh
+++ k8s/start-nginx.sh
@@ -2,9 +2,10 @@
 
 # Setting default environment variables
 export APP_INNTEKT_API_PATH="${APP_INNTEKT_API_PATH:-/http://dp-inntekt-api/v1/inntekt/uklassifisert}"
+export RESOLVER=$(cat /etc/resolv.conf | grep -v '^#' | grep -m 1 nameserver | awk '{print $2}') # Picking the first nameserver.
 
 # replace env for nginx conf
-envsubst '$APP_INNTEKT_API_PATH' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$APP_INNTEKT_API_PATH $RESOLVER' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 
 
 echo "Using APP_INNTEKT_API_PATH=${APP_INNTEKT_API_PATH}"
