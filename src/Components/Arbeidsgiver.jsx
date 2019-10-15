392974c72027f322fcafcfa0cbab2b63610f3104
Petter Ruud <petter.ruud@nav.no>
2019-10-15 14:02:32 +0200

    setter preprod tilbake til master

--- src/Components/Arbeidsgiver.jsx
+++ src/Components/Arbeidsgiver.jsx
@@ -3,7 +3,6 @@ import { Element, EtikettLiten, Undertittel } from 'nav-frontend-typografi';
 import aktørTyper from '../Kodeverk/aktoerTyper';
 import ArbeidsgiverPropType from '../PropTypes/arbeidsgiverPropType';
 
-
 const getAktoerType = aktoerType => (aktoerType === aktørTyper.ORGANISASJON ? 'Org. nr' : 'FNR');
 
 const arbeidsgiverStyle = identifikator => `
@@ -16,9 +15,11 @@ const Arbeidsgiver = ({ arbeidsgiver }) => {
   const { navn, identifikator, aktoerType } = arbeidsgiver;
   return (
     <>
-      <style dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-        __html: arbeidsgiverStyle(identifikator),
-      }}
+      <style
+        dangerouslySetInnerHTML={{
+          // eslint-disable-line react/no-danger
+          __html: arbeidsgiverStyle(identifikator),
+        }}
       />
       <div className={`item arbeidsgiver arbeidsgiver--${identifikator}`}>
         {navn && <Undertittel>{navn}</Undertittel>}
