0775566070e94732470e26c7a10966c06a13b8d0
Geir A. Lund <geir.andre.lund@nav.no>
2020-08-03 12:41:21 +0200

    Byttet ut oppslag mot dagpenger-oppslag med brreg enhetsregister (#94)
    * Byttet ut oppslag mot dagpenger-oppslag med brreg enhetsregister via inntekt-api
    navik/dagpenger#539
    
    * Feilmelding `Failed to get vulns` fra snyk. Denne https://nav-it.slack.com/archives/CN8N938K1/p1592469963116300 foreslår å flytte react-scripts til devDependencies
    
    * Update snyk

---
+++ src/lib/__tests__/brregApiClient.test.jsx
@@ -0,0 +1,26 @@
+import nock from 'nock';
+import { getOrganisasjonsNavn } from '../brregApiClient';
+
+const baseURL = 'http://localhost';
+
+
+const apiUri = '/enhetsregisteret/v1/enhetsregisteret/enhet';
+
+
+describe('BrregApiClient', () => {
+  it('Should handle 200 response on org name lookup', async () => {
+    const orgNr = 12345678;
+    const expectedReturn = { organisasjonsnummer: '12345678', navn: 'Et navn' };
+    nock(baseURL)
+      .get(`${apiUri}/${orgNr}`)
+      .reply(200, expectedReturn);
+
+    try {
+      const result = await getOrganisasjonsNavn(orgNr);
+      expect(result.data).toEqual(expectedReturn);
+    } catch (error) {
+      throw error;
+    }
+  });
+
+});
--- src/lib/brregApiClient.jsx
+++ src/lib/brregApiClient.jsx
@@ -1,28 +1,15 @@
 import axios from 'axios';
 
-const apiUri = '/oppslag/api';
+const apiUri = '/enhetsregisteret/v1/enhetsregisteret/enhet';
 
 export const getOrganisasjonsNavn = async (orgNr) => {
   try {
     return await axios({
       method: 'get',
-      url: `${apiUri}/organisasjon/${orgNr}`,
+      url: `${apiUri}/${orgNr}`,
       validateStatus(status) {
         return status < 500; // Reject only if the status code is greater than or equal to 500
       },
-    });
-  } catch (error) {
-    return error;
-  }
-};
-
-
-export const getPersonNavn = async (request) => {
-  try {
-    return await axios({
-      method: 'post',
-      url: `${apiUri}/person/name`,
-      data: request,
     });
   } catch (error) {
     return error;
